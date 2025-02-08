c9e54e466f14b8c5e3e0bf6ba5030775862ed041
Sue Feng Tan <suefengtan@gmail.com>
2025-02-08 15:59:41 -0600

    update schema for user types

--- db/schema.rb
+++ db/schema.rb
@@ -10,12 +10,18 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2025_02_08_215234) do
+ActiveRecord::Schema[7.1].define(version: 2025_02_08_215753) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
   create_table "groups", force: :cascade do |t|
     t.string "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "user_types", force: :cascade do |t|
+    t.string "role"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
