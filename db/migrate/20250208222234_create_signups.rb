50249388c1b3e41d645a9053de123fbe65ee0ecf
QA ninja <qa_ninja@live.com>
2025-03-08 19:40:01 -0600

    update event info

--- db/migrate/20250208222234_create_signups.rb
+++ db/migrate/20250208222234_create_signups.rb
@@ -4,11 +4,13 @@ class CreateSignups < ActiveRecord::Migration[7.1]
       t.integer :user_id, null: false
       t.integer :event_id, null: false
       t.text :volunteer_notes
-      t.text :post_event_coordinator_notes
       t.datetime :checked_in_at
       t.datetime :cancelled_at
 
       t.timestamps
     end
+
+    add_foreign_key :signups, :users, column: :user_id
+    add_foreign_key :signups, :event_infos, column: :event_id
   end
 end
