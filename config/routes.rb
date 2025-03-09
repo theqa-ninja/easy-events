445c42ef9505e80b94c3abbffe6db3c9a6baa87b
Sue Feng Tan <suefengtan@gmail.com>
2025-03-08 20:53:51 -0600

    Devise login/logout, with email templates

--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,8 @@ Rails.application.routes.draw do
   resources :signups
   resources :event_infos
   resources :user_types
-  devise_for :users
+  devise_for :users, :path => '', :path_names => {:sign_in => "login", :sign_out => "logout", :sign_up => "register" }
+
   resources :users
   resources :teams
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
