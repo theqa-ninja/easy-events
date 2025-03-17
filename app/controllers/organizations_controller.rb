30d0c9e3d7f19e46d817816d0c1e9e15cb8e823b
QA ninja <qa_ninja@live.com>
2025-03-16 22:10:48 -0500

    added organization link

--- app/controllers/organizations_controller.rb
+++ app/controllers/organizations_controller.rb
@@ -1,61 +1,62 @@
 class OrganizationsController < ApplicationController
-  before_action :set_organization, only: %i[ show edit update destroy ]
+  before_action :set_organization
+  before_action :redirect_if_not_leader
 
   # GET /organizations or /organizations.json
-  def index
-    @organizations = Organization.all
-  end
+  # def index
+  #   @organizations = Organization.all
+  # end
 
   # GET /organizations/1 or /organizations/1.json
   def show
   end
 
   # GET /organizations/new
-  def new
-    @organization = Organization.new
-  end
+  # def new
+  #   @organization = Organization.new
+  # end
 
   # GET /organizations/1/edit
   def edit
   end
 
   # POST /organizations or /organizations.json
-  def create
-    @organization = Organization.new(organization_params)
+  # def create
+  #   @organization = Organization.new(organization_params)
 
-    respond_to do |format|
-      if @organization.save
-        format.html { redirect_to @organization, notice: "Organization was successfully created." }
-        format.json { render :show, status: :created, location: @organization }
-      else
-        format.html { render :new, status: :unprocessable_entity }
-        format.json { render json: @organization.errors, status: :unprocessable_entity }
-      end
-    end
-  end
+  #   respond_to do |format|
+  #     if @organization.save
+  #       format.html { redirect_to @organization, notice: "Organization was successfully created." }
+  #       format.json { render :show, status: :created, location: @organization }
+  #     else
+  #       format.html { render :new, status: :unprocessable_entity }
+  #       format.json { render json: @organization.errors, status: :unprocessable_entity }
+  #     end
+  #   end
+  # end
 
   # PATCH/PUT /organizations/1 or /organizations/1.json
-  def update
-    respond_to do |format|
-      if @organization.update(organization_params)
-        format.html { redirect_to @organization, notice: "Organization was successfully updated." }
-        format.json { render :show, status: :ok, location: @organization }
-      else
-        format.html { render :edit, status: :unprocessable_entity }
-        format.json { render json: @organization.errors, status: :unprocessable_entity }
-      end
-    end
-  end
+  # def update
+  #   respond_to do |format|
+  #     if @organization.update(organization_params)
+  #       format.html { redirect_to @organization, notice: "Organization was successfully updated." }
+  #       format.json { render :show, status: :ok, location: @organization }
+  #     else
+  #       format.html { render :edit, status: :unprocessable_entity }
+  #       format.json { render json: @organization.errors, status: :unprocessable_entity }
+  #     end
+  #   end
+  # end
 
   # DELETE /organizations/1 or /organizations/1.json
-  def destroy
-    @organization.destroy!
+  # def destroy
+  #   @organization.destroy!
 
-    respond_to do |format|
-      format.html { redirect_to organizations_path, status: :see_other, notice: "Organization was successfully destroyed." }
-      format.json { head :no_content }
-    end
-  end
+  #   respond_to do |format|
+  #     format.html { redirect_to organizations_path, status: :see_other, notice: "Organization was successfully destroyed." }
+  #     format.json { head :no_content }
+  #   end
+  # end
 
   private
     # Use callbacks to share common setup or constraints between actions.
@@ -66,5 +67,9 @@ class OrganizationsController < ApplicationController
     # Only allow a list of trusted parameters through.
     def organization_params
       params.fetch(:organization, {})
+    end
+
+    def redirect_if_not_leader
+      redirect_to event_infos_path if !current_user.is_leader
     end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,28 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  def current_organization
+    @current_organization
+  end
+
+  def current_team
+    @current_team
+  end
+
+  def is_leader
+    result = UsersTypesTeam.find_by(user_id: self.id)
+    return false if result.nil?
+    @current_organization = Organization.find_by(id: result.organization_id)
+    @current_team = Team.find_by(id: result.team_id)
+    self && UsersTypesTeam.find_by(user_id: self.id)
+  end
+
+  def is_admin
+    result = UsersTypesTeam.find_by(user_id: self.id)
+    return false if result.nil?
+    @current_organization = Organization.find_by(id: result.organization_id)
+    @current_team = Team.find_by(id: result.team_id)
+    result&.user_type == "admin"
+  end
 end
