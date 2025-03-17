b5150fb092782a1e03b04ad23fa6b64c559c3052
Fiona Tang <ftang@codeforamerica.org>
2025-03-16 19:03:22 -0500

    add backend validations for email, name, and whether over 18

--- app/models/signup.rb
+++ app/models/signup.rb
@@ -4,6 +4,9 @@ class Signup < ApplicationRecord
 
   # when submitting a form, preserves scroll position with turbo broadcasts
   # goes with turbo_stream_from signup
-  broadcasts_refreshes 
-  
+  broadcasts_refreshes
+
+  # Every saved signup must have name, email address, and whether the user is over 18
+  validates :user_name, :user_email, :user_is_over_18, presence: true
+  validates :user_email, uniqueness: true
 end
