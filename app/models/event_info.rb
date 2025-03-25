91254a94f66bff528177fe8437e58cd37a20908a
QA ninja <qa_ninja@live.com>
2025-03-24 22:37:00 -0500

    added volunteer_role_counts

--- app/models/event_info.rb
+++ app/models/event_info.rb
@@ -21,6 +21,17 @@ class EventInfo < ApplicationRecord
     "full"
   end
 
+  def volunteer_role_counts
+    # get all the unique volunteer roles for this event
+    signups = Signup.where(event_id: id)
+    # query the sign ups
+    roles = VolunteerRole.where(team_id: team_id)
+    # return a hash of role => count
+    roles.map do |role|
+      {"role": role.role, "role_id": role.id, "count": signups.where(volunteer_role_id: role.id).count}
+    end.to_ary
+  end
+
   # def as_json(options = {})
   #   super(options).merge(remaining_adult_slots: remaining_adult_slots).merge(remaining_teenager_slots: remaining_teenager_slots)
   # end
