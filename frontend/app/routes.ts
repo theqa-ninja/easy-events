74f964ff4112bc9d20a600761733007949ecd4e4
Sue <suefengtan@gmail.com>
2025-04-29 18:54:13 -0500

    Update path to "/login"

--- frontend/app/routes.ts
+++ frontend/app/routes.ts
@@ -2,5 +2,5 @@ import { type RouteConfig, index } from "@react-router/dev/routes";
 
 export default [
     index("routes/home.tsx"), 
-    { path: "login", file: "routes/login.tsx" }
+    { path: "/login", file: "routes/login.tsx" }
 ] satisfies RouteConfig;
