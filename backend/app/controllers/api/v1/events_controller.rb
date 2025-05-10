ce5585a98a30f51a185ede1ed42c435f2b177841
QA ninja <qa_ninja@live.com>
2025-05-10 08:48:58 -0500

    cleaned up events more

--- backend/app/controllers/api/v1/events_controller.rb
+++ backend/app/controllers/api/v1/events_controller.rb
@@ -3,7 +3,7 @@ module Api
     class EventsController < ApplicationController
       before_action :set_permissions
       before_action :redirect_if_not_admin, only: %i[ signups check_ins create update destroy ]
-      before_action :set_event, only: %i[ show signup signups check_ins edit update destroy ]
+      before_action :set_event, only: %i[ show signups check_ins update destroy ]
 
       # GET /events or /events.json
       def index
@@ -19,22 +19,28 @@ module Api
 
       # GET /events/1/signup
       def signup
-        # TODO: redirect to signup api call?
+        # TODO
+        # check if event is full
+        # check if user is already signed up
+        # create new signup entry if not
+        # render json if signup.save passes or not
       end
 
       # GET /events/1/signups
       def signups
-        @signups_over_18 = Signup.where(soft_deleted: false).where(event_id: @event.id, user_is_over_18: true).order(:user_name)
-        @signups_under_18 = Signup.where(soft_deleted: false).where(event_id: @event.id, user_is_over_18: false).order(:user_name)
+        signups_over_18 = Signup.where(soft_deleted: false).where(event_id: @event.id, user_is_over_18: true).order(:user_name)
+        signups_under_18 = Signup.where(soft_deleted: false).where(event_id: @event.id, user_is_over_18: false).order(:user_name)
         # @signups = Signup.where(event_id: @event.id).order(:user_name) # for no API
-        render json: { adults: @signups_over_18.as_json, under_18: @signups_under_18.as_json }, status: :ok
+        render json: { adults: {filled: signups_over_18.length, signups: signups_over_18.as_json},
+          under_18: {filled: signups_under_18.length, signups: signups_under_18.as_json}
+        }, status: :ok
       end
 
       # GET /events/1/check-ins
       def check_ins
-        @checkins_over_18 = Signup.where(event_id: @event.id, user_is_over_18: true).where.not(checked_in_at: nil).order(:user_name)
-        @checkins_under_18 = Signup.where(event_id: @event.id, user_is_over_18: false).where.not(checked_in_at: nil).order(:user_name)
-        render json: { adults: @checkins_over_18.as_json, under_18: @checkins_under_18.as_json }, status: :ok
+        checkins_over_18 = Signup.where(event_id: @event.id, user_is_over_18: true).where.not(checked_in_at: nil).order(:user_name)
+        checkins_under_18 = Signup.where(event_id: @event.id, user_is_over_18: false).where.not(checked_in_at: nil).order(:user_name)
+        render json: { adults: checkins_over_18.as_json, under_18: checkins_under_18.as_json }, status: :ok
       end
 
       # POST /events or /events.json
@@ -67,7 +73,8 @@ module Api
 
       # Use callbacks to share common setup or constraints between actions.
       def set_event
-        @event = Event.where(soft_deleted: false).where(id: params[:id])
+        @event = Event.where(soft_deleted: false).where(id: params[:id]).first
+        render json: { message: 'Event not found' }, status: :not_found if @event.nil?
       end
 
       # Only allow a list of trusted parameters through.
@@ -81,7 +88,7 @@ module Api
 
       def redirect_if_not_admin
         # redirect_to events_path if !@user_is_event_coordinator_or_admin
-        render json: { message: 'You are not high enough to do that' }, status: :unauthorized
+        render json: { message: 'You are not high enough to do that' }, status: :unauthorized if @user_is_event_coordinator_or_admin
       end
     end
   end
