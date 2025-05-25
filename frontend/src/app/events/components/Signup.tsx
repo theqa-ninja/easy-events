d4524272fdc3836d743691d77e673d51b9dfe8fa
Sue <suefengtan@gmail.com>
2025-05-25 17:39:07 -0500

    Get createSignup started, needs login to work in order to use it

--- frontend/src/app/events/components/Signup.tsx
+++ frontend/src/app/events/components/Signup.tsx
@@ -1,11 +1,12 @@
-import { getSignups, ISignup } from "../events.service";
+'use client';
+import { createSignup, getSignups, ISignup } from "../events.service";
 import { Input } from "../../components/Input";
 import { Textarea } from "../../components/Textarea";
 import { BinaryRadioInput } from "@/app/components/BinaryRadioInput";
 import { Button } from "@/app/components/Button";
 
-export const Signup = async ({ eventId } : { eventId: string }) => {
-    const signupsData = await getSignups(eventId);
+export const Signup = ({ eventId } : { eventId: string }) => {
+    const signupsData = getSignups(eventId);
     const userId = 1;
     const signupData = signupsData && signupsData?.length > 0 && signupsData.find(signup => signup.user_id === userId);
     const signup: ISignup = {
@@ -17,8 +18,14 @@ export const Signup = async ({ eventId } : { eventId: string }) => {
         user_is_over_18: signupData && signupData?.user_is_over_18 || false,
         notes: signupData && signupData?.notes || '',
     };
+    const submitSignup =  (event: React.FormEvent<HTMLFormElement>) => {
+        event.preventDefault();
+        const formData = new FormData(event.currentTarget);
+        const body = JSON.stringify(Object.fromEntries(formData));
+        createSignup(eventId, JSON.parse(body));
+    }
     return (
-        <form className="flex flex-col gap-4 w-100">
+        <form onSubmit={submitSignup} className="flex flex-col gap-4 w-100">
             <Input type="text" name="name" placeholder="Name" defaultValue={signup.user_name} />
             <Input type="email" name="email" placeholder="Email" defaultValue={signup.user_email} />
             <Input type="tel" name="phone_number" placeholder="Phone number" defaultValue={signup.user_phone_number} />
