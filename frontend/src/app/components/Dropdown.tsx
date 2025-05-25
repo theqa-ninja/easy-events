084d089f160cf33c6848b84817148da907eaf958
Sue <suefengtan@gmail.com>
2025-05-25 13:51:16 -0500

    Remove JSX.Element

--- frontend/src/app/components/Dropdown.tsx
+++ frontend/src/app/components/Dropdown.tsx
@@ -21,7 +21,7 @@ export const DropDown: React.FC<DropDownProps> = ({
         className="block w-full p-2 border border-neutral-300 rounded-md shadow-inner active:outline-none active:ring-2 active:ring-primary-600 active:ring-offset-2 bg-background"
       >
         <option value="">{helpText}</option>
-        {choices.map((choice: string, index: number): JSX.Element => {
+        {choices.map((choice: string, index: number) => {
           return (
             <option key={index} value={choice}>
               {choice}
