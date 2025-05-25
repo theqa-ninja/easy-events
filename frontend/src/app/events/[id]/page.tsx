5c418618aa84b982310fd7a36b4cc4302d981d46
Sue <suefengtan@gmail.com>
2025-05-25 17:33:04 -0500

    Use event and signup components

--- frontend/src/app/events/[id]/page.tsx
+++ frontend/src/app/events/[id]/page.tsx
@@ -1,45 +1,22 @@
 import React from "react";
-import { Button } from "../../components/Button";
-import { formatDateTime } from "../../utilities";
+import { getEvent, getSignups, ISignup, IEvent } from "../events.service";
+import { Event } from "../components/Event";
+import { Signup } from "../components/Signup";
 
 const EventDetails = async ({params}:{
   params: Promise<{ id: string }>
 }) => {
   const { id } = await params;
-  const data:any = await fetch(`http://localhost:3000/api/v1/events/${id}`, {
-    method: "GET",
-    headers: {
-      "Content-Type": "application/json",
-    },
-  }).then((response) => response.json())
-    .then((data) => data)
-    .catch((error) => error);
+  const eventData = await getEvent(id);
 
   return (
     <main className="flex flex-col items-center justify-between p-4 max-w-4xl m-auto">
-      {data && (
-        <div key={data.id}>
-          <h1>{data.title}</h1>
-          <p>{data.description}</p>
-          <dl>
-            <dt>Start time</dt>
-            <dd>{formatDateTime(data.start_time)}</dd>
-          </dl>
-          <dl>
-            <dt>End time</dt>
-            <dd>{formatDateTime(data.end_time)}</dd>
-          </dl>
-          <dl>
-            <dt>Adult slots</dt>
-            <dd>{data.adult_slots}</dd>
-          </dl>
-          <dl>
-            <dt>Teenager slots</dt>
-            <dd>{data.teenager_slots}</dd>
-          </dl>
-        </div>
+      {eventData && eventData && (
+        <Event eventData={eventData} />
       )}
-      <Button label="Sign up here"></Button>
+      {eventData && (
+        <Signup eventId={id} />
+      )}
     </main>
   );
 };
