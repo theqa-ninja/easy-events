c6464d50e377d3302ef4ca39c88f79ebfe9ecd62
Sue <suefengtan@gmail.com>
2025-05-26 15:50:36 -0500

    Update event signup to have logged in check for message to show user

--- frontend/src/app/events/[id]/signup/page.tsx
+++ frontend/src/app/events/[id]/signup/page.tsx
@@ -2,7 +2,8 @@ import React from "react";
 import { getEvent, getSignup, ISignup } from "../../events.service";
 import { Event } from "../../components/Event";
 import { SignupForm } from "../../components/SignupForm";
-import { cookies } from "next/headers";
+import { validateToken } from "@/app/utilities";
+import Link from "next/link";
 
 const EventDetails = async ({
   params,
@@ -12,20 +13,30 @@ const EventDetails = async ({
   const { id } = await params;
   const eventData = await getEvent(id);
   const signupData = await getSignup(id);
-  const userId = signupData && signupData?.user_id || 1;
+  const userId = (signupData && signupData?.user_id) || 1;
   const signup: ISignup = {
-      event_id: Number(id),
-      user_id: Number(userId),
-      user_name: signupData && signupData?.user_name || '',
-      user_email: signupData && signupData?.user_email || '',
-      user_phone_number: signupData && signupData?.user_phone_number || '',
-      user_is_over_18: signupData && signupData?.user_is_over_18 || false,
-      notes: signupData && signupData?.notes || '',
+    event_id: Number(id),
+    user_id: Number(userId),
+    user_name: (signupData && signupData?.user_name) || "",
+    user_email: (signupData && signupData?.user_email) || "",
+    user_phone_number: (signupData && signupData?.user_phone_number) || "",
+    user_is_over_18: (signupData && signupData?.user_is_over_18) || false,
+    notes: (signupData && signupData?.notes) || "",
   };
+
+  const loggedIn = await validateToken();
 
   return (
     <main className="flex flex-col items-center justify-between p-4 max-w-4xl m-auto">
       {eventData && <Event eventData={eventData} />}
+      <h2>Signup for this event</h2>
+      {!loggedIn && (
+        <p>
+          Would you like to <Link href="/user/login">log in</Link> or{" "}
+          <Link href="/user/create-account">create an account</Link> to save
+          your account to signup more quickly in the future?
+        </p>
+      )}
       {eventData && <SignupForm signup={signup} eventId={eventData.id} />}
     </main>
   );
