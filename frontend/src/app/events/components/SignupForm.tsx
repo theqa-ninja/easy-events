36fab8a3d046fc8a38f00654ef258b83d71ba06b
Sue <suefengtan@gmail.com>
2025-05-26 15:04:17 -0500

    Add form validation for signup form and make submit with toast messages

--- frontend/src/app/events/components/SignupForm.tsx
+++ frontend/src/app/events/components/SignupForm.tsx
@@ -5,6 +5,9 @@ import { Input } from "../../components/Input";
 import { Textarea } from "../../components/Textarea";
 import { BinaryRadioInput } from "@/app/components/BinaryRadioInput";
 import { Button } from "@/app/components/Button";
+import { Toast } from "@/app/components/Toast";
+import { object, string } from "yup";
+import { validateOnBlur } from "@/app/utilities";
 
 export const SignupForm = ({
   signup,
@@ -13,57 +16,102 @@ export const SignupForm = ({
   signup: ISignup;
   eventId: number;
 }) => {
-  const [message, setMessage] = useState("");
+  const [toast, setToast] = useState<{
+    message: string;
+    status: "success" | "error";
+  }>();
+
+  const [errors, setErrors] = useState<{ [name: string]: string }>({});
+  const signupSchema = object({
+    name: string().required("Name is required"),
+    email: string().email("Invalid email").required("Email is required"),
+    is_over_18: string().required("Age is required"),
+  });
+
+  const handleChange = async (event: React.ChangeEvent<HTMLInputElement>) => {
+    validateOnBlur(event, signupSchema, setErrors);
+  };
+
   const submitSignup = (event: React.FormEvent<HTMLFormElement>) => {
     event.preventDefault();
     const formData = new FormData(event.currentTarget);
     const body = JSON.stringify(Object.fromEntries(formData));
-    createSignup(eventId, JSON.parse(body))
+
+    try {
+      signupSchema.validateSync(Object.fromEntries(formData), {
+        abortEarly: false,
+      });
+      setErrors({});
+      createSignup(eventId, JSON.parse(body))
       .then(() => {
-        setMessage("Signup successful");
+        setToast({ message: "Signup successful", status: "success" });
       })
       .catch(() => {
-        setMessage("Signup failed");
+        setToast({ message: "Signup failed", status: "error" });
       });
-    // TODO: show success or error message in Toast
+    } catch (validationError: any) {
+      const formattedError = validationError.inner.reduce(
+        (acc: any, err: any) => {
+          acc[err.path] = err.message;
+          return acc;
+        },
+        {}
+      );
+      setErrors(formattedError);
+    }
   };
   return (
-    <form onSubmit={submitSignup} className="flex flex-col gap-4 w-100">
-      <Input
-        type="text"
-        name="user_name"
-        placeholder="Name"
-        defaultValue={signup.user_name}
-      />
-      <Input
-        type="email"
-        name="user_email"
-        placeholder="Email"
-        defaultValue={signup.user_email}
-      />
-      <Input
-        type="tel"
-        name="user_phone_number"
-        placeholder="Phone number"
-        defaultValue={signup.user_phone_number}
-      />
-      <BinaryRadioInput
-        name="user_is_over_18"
-        idA="yes"
-        idB="no"
-        labelA="Yes"
-        labelB="No"
-        valueA="true"
-        valueB="false"
-        question="Are you over 18 years old?"
-      />
-      <Textarea
-        name="notes"
-        placeholder="notes..."
-        defaultValue={signup.notes}
-      />
+    <>
+      {toast && (
+        <Toast
+          message={toast.message}
+          status={toast.status}
+          onClose={() => setToast(undefined)}
+        />
+      )}
+      <form onSubmit={submitSignup} className="flex flex-col gap-4 w-100">
+        <Input
+          type="text"
+          name="name"
+          placeholder="Name"
+          defaultValue={signup.user_name}
+          onBlur={handleChange}
+          errorMessage={errors.name}
+        />
+        <Input
+          type="email"
+          name="email"
+          placeholder="Email"
+          defaultValue={signup.user_email}
+          onBlur={handleChange}
+          errorMessage={errors.email}
+        />
+        <Input
+          type="tel"
+          name="phone_number"
+          placeholder="Phone number"
+          defaultValue={signup.user_phone_number}
+        />
+        <BinaryRadioInput
+          name="is_over_18"
+          idA="yes"
+          idB="no"
+          labelA="Yes"
+          labelB="No"
+          valueA="true"
+          valueB="false"
+          question="Are you over 18 years old?"
+          onBlur={handleChange}
+          errorMessage={errors.is_over_18}
+        />
+        <Textarea
+          name="notes"
+          placeholder="notes..."
+          defaultValue={signup.notes}
+        />
 
-      <Button type="submit" label="Sign up" />
-    </form>
+        <Button type="submit" label="Sign up" />
+      </form>
+    </>
   );
 };
