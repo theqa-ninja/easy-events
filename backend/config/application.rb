89c19bb5b7d3402177ccc9ace30019afdf3e475e
QA ninja <qa_ninja@live.com>
2025-05-25 21:00:31 -0500

    moving to api only to avoid CSRF issues

--- backend/config/application.rb
+++ backend/config/application.rb
@@ -23,5 +23,8 @@ module EasyEvents
     #
     config.time_zone = "Central Time (US & Canada)"
     # config.eager_load_paths << Rails.root.join("extras")
+
+    # Set the application to API-only mode
+    config.api_only = true
   end
 end
