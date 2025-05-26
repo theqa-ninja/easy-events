ec349145fc17d8ae1cf881e330ca0fca48d495d2
Sue <suefengtan@gmail.com>
2025-05-26 10:37:45 -0500

    Add signups for event

---
+++ frontend/src/app/events/[id]/signups/page.tsx
@@ -0,0 +1,24 @@
+import React from "react";
+import { getSignups } from "../../events.service";
+import { SignupsTable } from "../../components/SignupsTable";
+
+const EventDetails = async ({
+  params,
+}: {
+  params: Promise<{ id: string }>;
+}) => {
+  const { id } = await params;
+  const signupsData = await getSignups(id);
+
+  return (
+    <main className="flex flex-col items-center justify-between p-4">
+      <h1>Signups</h1>
+      <h2>Adults</h2>
+      <SignupsTable signupsData={signupsData.adults.signups} />
+      <h2 className="mt-8">Under 18</h2>
+      <SignupsTable signupsData={signupsData.under_18.signups} />
+    </main>
+  );
+};
+
+export default EventDetails;
---
+++ frontend/src/app/events/components/SignupsTable.tsx
@@ -0,0 +1,34 @@
+import { ISignup } from "../events.service";
+
+export const SignupsTable = ({ signupsData }: { signupsData: ISignup[] }) => {
+  return (
+    <table className="w-full table-fixed">
+      <thead>
+        <tr className="bg-foreground-100">
+          <th>Name</th>
+          <th>Email</th>
+          <th>Phone number</th>
+          <th>Checked in at</th>
+          <th>Cancelled at</th>
+        </tr>
+      </thead>
+      <tbody>
+        {signupsData &&
+          Object.values(signupsData).map((signup) => (
+            <>
+              <tr key={signup.id}>
+                <td className="font-bold">{signup.user_name}</td>
+                <td>{signup.user_email}</td>
+                <td>{signup.user_phone_number}</td>
+                <td>{signup.checked_in_at}</td>
+                <td>{signup.cancelled_at}</td>
+              </tr>
+              <tr className="border-b-1 border-primary-900">
+                <td colSpan={5}>{signup.notes && <b>Notes from volunteer: </b>}{signup.notes}</td>
+              </tr>
+            </>
+          ))}
+      </tbody>
+    </table>
+  );
+};
