0fde3397ee358099917d3c7c754a494ed25ec7a7
QA ninja <qa_ninja@live.com>
2025-05-26 14:38:15 -0500

    added postman docs

--- backend/app/controllers/api/v1/events_controller.rb
+++ backend/app/controllers/api/v1/events_controller.rb
@@ -17,6 +17,50 @@ module Api
         render json: @event.as_json, status: :ok
       end
 
+      # GET /events/1/signups
+      def signups
+        signups_over_18 = Signup.where(soft_deleted: false).where(event_id: @event.id, user_is_over_18: true).order(:user_name)
+        signups_under_18 = Signup.where(soft_deleted: false).where(event_id: @event.id, user_is_over_18: false).order(:user_name)
+        # @signups = Signup.where(event_id: @event.id).order(:user_name) # for no API
+        render json: { adults: {filled: signups_over_18.length, signups: signups_over_18.as_json},
+          under_18: {filled: signups_under_18.length, signups: signups_under_18.as_json}
+        }, status: :ok
+      end
+
+      # GET /events/1/checkins
+      def check_ins
+        checkins_over_18 = Signup.where(event_id: @event.id, user_is_over_18: true).where.not(checked_in_at: nil).order(:user_name)
+        checkins_under_18 = Signup.where(event_id: @event.id, user_is_over_18: false).where.not(checked_in_at: nil).order(:user_name)
+        render json: { adults: checkins_over_18.as_json, under_18: checkins_under_18.as_json }, status: :ok
+      end
+
+      # POST /events or /events.json
+      def create
+        @event = Event.new(event_params)
+        if @event.save
+          render json: @event, status: :created
+        else
+          render json: @event.errors, status: :unprocessable_entity
+        end
+      end
+
+      # PATCH/PUT /events/1 or /events/1.json
+      def update
+        if @event.update(event_params)
+          render json: @event, status: :ok
+        else
+          render json: @event.errors, status: :unprocessable_entity
+        end
+      end
+
+      # DELETE /events/1 or /events/1.json
+      def destroy
+        @event.delete
+
+        render json: @event, status: :accepted
+      end
+
+
       # POST /events/1/signup
       def create_signup
         # check if event is full
@@ -76,49 +120,6 @@ module Api
         # return the deleted signup
       end
 
-      # GET /events/1/signups
-      def signups
-        signups_over_18 = Signup.where(soft_deleted: false).where(event_id: @event.id, user_is_over_18: true).order(:user_name)
-        signups_under_18 = Signup.where(soft_deleted: false).where(event_id: @event.id, user_is_over_18: false).order(:user_name)
-        # @signups = Signup.where(event_id: @event.id).order(:user_name) # for no API
-        render json: { adults: {filled: signups_over_18.length, signups: signups_over_18.as_json},
-          under_18: {filled: signups_under_18.length, signups: signups_under_18.as_json}
-        }, status: :ok
-      end
-
-      # GET /events/1/check-ins
-      def check_ins
-        checkins_over_18 = Signup.where(event_id: @event.id, user_is_over_18: true).where.not(checked_in_at: nil).order(:user_name)
-        checkins_under_18 = Signup.where(event_id: @event.id, user_is_over_18: false).where.not(checked_in_at: nil).order(:user_name)
-        render json: { adults: checkins_over_18.as_json, under_18: checkins_under_18.as_json }, status: :ok
-      end
-
-      # POST /events or /events.json
-      def create
-        @event = Event.new(event_params)
-        if @event.save
-          render json: @event, status: :created
-        else
-          render json: @event.errors, status: :unprocessable_entity
-        end
-      end
-
-      # PATCH/PUT /events/1 or /events/1.json
-      def update
-        if @event.update(event_params)
-          render json: @event, status: :ok
-        else
-          render json: @event.errors, status: :unprocessable_entity
-        end
-      end
-
-      # DELETE /events/1 or /events/1.json
-      def destroy
-        @event.destroy!
-
-        render json: @event, status: :accepted
-      end
-
       private
 
       # Use callbacks to share common setup or constraints between actions.
@@ -133,11 +134,11 @@ module Api
       end
 
       def set_permissions
+        # @user_is_event_coordinator_or_admin = true # uncomment to test authorized areas
         @user_is_event_coordinator_or_admin = current_user && UsersTypesTeam.find_by(user_id: current_user.id)
       end
 
       def redirect_if_not_admin
-        # redirect_to events_path if !@user_is_event_coordinator_or_admin
         render json: { message: 'You are not high enough to do that' }, status: :unauthorized if !@user_is_event_coordinator_or_admin
       end
     end
