44095a3a222909544369255372807c85b2a8f2f0
Sue <suefengtan@gmail.com>
2025-06-07 16:09:31 -0500

    Update organizations and teams to be unordered lists

--- frontend/src/app/organizations/page.tsx
+++ frontend/src/app/organizations/page.tsx
@@ -1,24 +1,24 @@
 import { getOrganizations } from "./organizations.service";
 
 const OrganizationsPage = async () => {
-    const organizationsData = await getOrganizations();
-    console.log(organizationsData);
+  const organizationsData = await getOrganizations();
+  console.log(organizationsData);
 
-    return organizationsData && organizationsData.length > 0 ? (
-        <main className="flex flex-col items-center justify-between p-4">
-            <h1>Organizations</h1>
-            {organizationsData.map((organization) => (
-                <div key={organization.id} className="bg-white p-4 rounded shadow">
-                    <h2 className="text-xl font-bold">{organization.name}</h2>
-                </div>
-            ))}
-        </main>
-    ) : (
-        <main className="flex flex-col items-center justify-between p-4">
-            <h1>Organizations</h1>
-            <p>No organizations found.</p>
-        </main>
-    );
+  return organizationsData && organizationsData.length > 0 ? (
+    <main className="flex flex-col items-center justify-between p-4">
+      <h1>Organizations</h1>
+      <ul>
+        {organizationsData.map((organization) => (
+          <li className="text-xl font-bold" key={organization.id}>{organization.name}</li>
+        ))}
+      </ul>
+    </main>
+  ) : (
+    <main className="flex flex-col items-center justify-between p-4">
+      <h1>Organizations</h1>
+      <p>You need to log in to see your organizations or you do not have permission to view them.</p>
+    </main>
+  );
 };
 
 export default OrganizationsPage;
--- frontend/src/app/teams/page.tsx
+++ frontend/src/app/teams/page.tsx
@@ -1,24 +1,28 @@
 import { getTeams } from "./teams.service";
 
 const TeamsPage = async () => {
-    const teamsData = await getTeams();
-    console.log(teamsData);
+  const teamsData = await getTeams();
 
-    return teamsData && teamsData.length > 0 ? (
-        <main className="flex flex-col items-center justify-between p-4">
-            <h1>Teams</h1>
-            {teamsData.map((team) => (
-                <div key={team.id} className="bg-white p-4 rounded shadow">
-                    <h2 className="text-xl font-bold">{team.name}</h2>
-                </div>
-            ))}
-        </main>
-    ) : (
-        <main className="flex flex-col items-center justify-between p-4">
-            <h1>Teams</h1>
-            <p>No teams found.</p>
-        </main>
-    );
+  return teamsData && teamsData.length > 0 ? (
+    <main className="flex flex-col items-center justify-between p-4">
+      <h1>Teams</h1>
+      <ul>
+        {teamsData.map((team) => (
+          <li className="text-xl font-bold" key={team.id}>
+            {team.name}
+          </li>
+        ))}
+      </ul>
+    </main>
+  ) : (
+    <main className="flex flex-col items-center justify-between p-4">
+      <h1>Teams</h1>
+      <p>
+        You need to log in to see your teams or you do not have permission to
+        view them.
+      </p>
+    </main>
+  );
 };
 
 export default TeamsPage;
