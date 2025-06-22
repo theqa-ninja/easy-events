562ad27f0ad408c985dfde3901be58e242235593
Fiona Tang <ftang@codeforamerica.org>
2025-06-22 10:31:01 -0500

    add EventLinks to /events

--- frontend/src/app/events/[id]/EventLinks.tsx
+++ frontend/src/app/events/[id]/EventLinks.tsx
@@ -2,12 +2,18 @@ import { doesUserHavePermissions } from "@/app/user/users.service";
 import Link from "next/link";
 
 export const EventLinks = async ({ eventId }: { eventId: number }) => {
-  const userMayEditEvents = await doesUserHavePermissions(["Superadmin", "Admin", "Event Coordinator"]);
-  return userMayEditEvents && (
-    <div className="flex gap-4 mt-5">
-      <Link href={`/events/${eventId}/edit`}>Edit this event</Link> 
-      <Link href={`/events/${eventId}/signups`}>View signups</Link>
-      <Link href={`/events/${eventId}/check-ins`}>View check-ins</Link>
-    </div>
+  const userMayEditEvents = await doesUserHavePermissions([
+    "Superadmin",
+    "Admin",
+    "Event Coordinator",
+  ]);
+  return (
+    userMayEditEvents && (
+      <div className="flex gap-4 mt-5">
+        <Link href={`/events/${eventId}/edit`}>Edit this event</Link>
+        <Link href={`/events/${eventId}/signups`}>View signups</Link>
+        <Link href={`/events/${eventId}/check-ins`}>View check-ins</Link>
+      </div>
+    )
   );
 };
