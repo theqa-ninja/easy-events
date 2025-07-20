7c8325814cfb34f0e911a3883253fb2563960702
Sue <suefengtan@gmail.com>
2025-07-20 18:36:18 -0500

    Move getVolunteerRoles to teams service since it's tied to teams, and update the interfaces and api route for getVolunteerRoles

--- frontend/src/app/organizations/[id]/teams/teams.service.ts
+++ frontend/src/app/organizations/[id]/teams/teams.service.ts
@@ -1,10 +1,19 @@
 import { getToken } from "@/app/utilities";
-export interface IOrganization {
-    id: string;
-    name: string;
+export interface ITeam {
+  id: string;
+  name: string;
+  organization_id: string;
 }
 
-export const getTeams = async (organizationId: number): Promise<IOrganization[]> => {
+export interface IVolunteerRole {
+  id: number;
+  role: string;
+  team_id: number;
+}
+
+export const getTeams = async (
+  organizationId: number
+): Promise<ITeam[]> => {
   try {
     const token = await getToken();
     const headers: HeadersInit = {
@@ -12,12 +21,39 @@ export const getTeams = async (organizationId: number): Promise<IOrganization[]>
       Authorization: token || "",
     };
 
-    const res = await fetch(`${process.env.NEXT_PUBLIC_API_ROUTE}/organizations/${organizationId}/teams`, {
-      method: "GET",
-      headers: headers,
-    });
+    const res = await fetch(
+      `${process.env.NEXT_PUBLIC_API_ROUTE}/organizations/${organizationId}/teams`,
+      {
+        method: "GET",
+        headers: headers,
+      }
+    );
     const data = await res.json();
-    return data
+    return data;
+  } catch (error) {
+    throw error;
+  }
+};
+
+// TODO: update this when volunteer roles are implemented
+export const getVolunteerRoles = async (
+  teamId: number
+): Promise<IVolunteerRole[]> => {
+  try {
+    const token = await getToken();
+    const headers: HeadersInit = {
+      "Content-Type": "application/json",
+      Authorization: token || "",
+    };
+    const response = await fetch(
+      `${process.env.NEXT_PUBLIC_API_ROUTE}/teams/${teamId}/volunteer-roles`,
+      {
+        method: "GET",
+        headers,
+      }
+    );
+    const data = await response.json();
+    return data;
   } catch (error) {
     throw error;
   }
