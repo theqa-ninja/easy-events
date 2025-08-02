58e86973630d4711c63ebaaf95b5b04b258edf05
Sue <suefengtan@gmail.com>
2025-08-02 10:15:46 -0500

    Add ability to filter on the Homepage through passing searchParams to Events component

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -1,14 +1,24 @@
 import Events from "../app/events/page";
 import { getUser } from "@/app/user/users.service";
 
-export default async function Home() {
+const Home = async ({
+  searchParams,
+}: {
+  searchParams?: Promise<{
+    team_id?: number;
+    org_id?: number;
+    filter?: string;
+  }>;
+}) => {
   const user = await getUser();
 
   return (
     <>
       <h1>Welcome to Easy Events</h1>
       {user?.name && <h2>Hello {user.name}!</h2>}
-      <Events />
+      <Events searchParams={searchParams} />
     </>
   );
 }
+
+export default Home;
