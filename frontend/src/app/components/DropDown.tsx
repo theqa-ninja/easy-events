d3618f48d049c0121f3a818b04efc3f8129cbd3a
Sue <suefengtan@gmail.com>
2025-08-03 19:48:56 -0500

    Rename Dropdown

---
+++ frontend/src/app/components/DropDown.tsx
@@ -0,0 +1,55 @@
+import React from "react";
+import { ErrorMessage } from "./ErrorMessage";
+
+interface DropDownProps extends React.SelectHTMLAttributes<HTMLSelectElement> {
+  choices: string[] | { value: string; label: string }[];
+  helpText?: string;
+  label?: string;
+  defaultValue?: string;
+  onChange?: React.ChangeEventHandler<HTMLSelectElement>;
+  errorMessage?: string;
+}
+export const DropDown = ({
+  choices,
+  name,
+  helpText = "Please select an option",
+  label,
+  defaultValue,
+  onChange,
+  errorMessage,
+}: DropDownProps) => {
+  return (
+    <div className="flex flex-col mb-0">
+      {label && <label htmlFor={name}>{label}</label>}
+      <select
+        name={name}
+        id={name}
+        className="rounded-md bg-background border-1 not-dark:border-slate-300 dark:border-slate-700 p-2"
+        defaultValue={defaultValue}
+        onChange={onChange}
+      >
+        <option value="">{helpText}</option>
+        {choices.map(
+          (
+            choice: string | { value: string; label: string },
+            index: number
+          ) => {
+            if (choice instanceof Object) {
+              return (
+                <option key={index} value={choice.value}>
+                  {choice.label}
+                </option>
+              );
+            }
+            return (
+              <option key={index} value={choice}>
+                {choice}
+              </option>
+            );
+          }
+        )}
+      </select>
+      <ErrorMessage message={errorMessage} />
+    </div>
+  );
+};
